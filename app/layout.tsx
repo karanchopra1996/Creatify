9f01157a5a012b4302505fb64c421d8fcb633194
Karan Chopra <114980007+karanchopra1996@users.noreply.github.com>
2025-02-04 23:02:09 -0800

    Clerk integration for authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
 import type { Metadata } from "next";
-import { IBM_Plex_Sans} from "next/font/google";
-import "./globals.css";
+import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
 
-const IBMPlex = IBM_Plex_Sans({
+import "./globals.css";
+
+const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
-  weight: [ '400', '500', '600', '700'],
+  weight: ['400', '500', '600', '700'],
   variable: '--font-ibm-plex'
 });
 
 export const metadata: Metadata = {
   title: "Creatify",
-  description: "AI-powered image generator ",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
@@ -21,13 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-    <html lang="en">
-      <body
-        className={ cn("font-IBMPlex antialiased", IBMPlex.variable) }>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }} afterSignOutUrl='/'>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
